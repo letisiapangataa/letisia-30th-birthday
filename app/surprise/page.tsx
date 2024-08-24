c79ed58a646b3b30a6d93498e40e78f26ddf1fa3
Letisia Pangata'a <51559861+letisiapangataa@users.noreply.github.com>
2024-08-25 01:32:47 +1200

    Update page.tsx

--- app/surprise/page.tsx
+++ app/surprise/page.tsx
@@ -8,7 +8,7 @@ export default function Surprise() {
     <div className="home">
       <div className="home-content">
       <p className='bday-logo-sp'>
-      <Image src="/assets/unlock_the_surprise_logo.png" width={864.999} height={505} alt='Unlock The Surprise' />
+      <Image src="/assets/unlock_the_surpise_logo.png" width={864.999} height={505} alt='Unlock The Surprise'/>
       </p>
       <div className="bday-txt-sp">Thank you for accepting the invitation to celebrate my birthday!<br></br>Unlock the <b>secret</b> location below. </div>
       <Link className="bday-txt-sp" href="/location">
