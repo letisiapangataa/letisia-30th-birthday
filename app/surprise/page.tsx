2b8a42707f6038689fd95b531436520020e665d9
Letisia Pangata'a <51559861+letisiapangataa@users.noreply.github.com>
2024-08-25 19:05:07 +1200

    Update page.tsx

--- app/surprise/page.tsx
+++ app/surprise/page.tsx
@@ -5,34 +5,6 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 export default function Surprise() {
-  const [secondsLeft, setSecondsLeft] = useState(30); // Start countdown from 30 seconds
-  const [isCountdownFinished, setIsCountdownFinished] = useState(false);
-
-  useEffect(() => {
-    const countdownDate = new Date().getTime() + 30 * 1000; // Set countdown for 30 seconds from now
-
-    const startCountdown = () => {
-      const now = new Date().getTime();
-      const difference = Math.floor((countdownDate - now) / 1000);
-
-      if (difference <= 0) {
-        setIsCountdownFinished(true);
-        clearInterval(timerInterval);
-      } else {
-        setSecondsLeft(difference);
-      }
-    };
-
-    const timerInterval = setInterval(startCountdown, 1000);
-
-    // Clean up the interval on component unmount
-    return () => clearInterval(timerInterval);
-  }, []);
-
-  const formatTime = (time: number, unit: string): string => {
-    return time === 1 ? `${time} ${unit}` : `${time} ${unit}s`;
-  };
-
   return (
     <div className="home">
       <div className="home-content">
@@ -44,19 +16,9 @@ export default function Surprise() {
           <br />
           Unlock the <b>secret</b> location below.
         </div>
-        {!isCountdownFinished ? (
-          <div className="bday-txt-sp">
-            <span id="seconds" role="timer">
-              Loading in {formatTime(secondsLeft, "second")}
-            </span>
-          </div>
-        ) : (
-          <section id="content" className="visible">
             <Link className="bday-txt-sp" href="/location">
               <button className="unlock-location">Unlock Secret Location</button>
             </Link>
-          </section>
-        )}
         <Link href="/">
           <p></p>
           <button className="more-information-btn">GO BACK</button>
